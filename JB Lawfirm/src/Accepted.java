747fc7b8614a67ce15064c11b7aa42e75eb7af5a
Reenu <Reenu@Reenu-PC.eduroam.gmu.edu>
2015-11-08 12:29:33 -0500

    updated case stuff

---
+++ JB Lawfirm/src/Accepted.java
@@ -0,0 +1,37 @@
+
+public class Accepted extends Case {
+	
+	int numLawyers;
+	String courthouseName;
+	float numDaysTrial;
+	
+	
+	public Accepted(){
+		super();
+		numLawyers  = 0;
+		courthouseName = "";
+		numDaysTrial = 0;
+		
+	}
+	public void setNumLawyers(int num){
+		numLawyers = num;
+		
+	}
+	public void setCourthouseName(String court){
+		courthouseName = court;
+	}
+	public void setNumDaysTrial(float days){
+		numDaysTrial = days;
+	}
+	public int getNumLawyers(){
+		return numLawyers;
+	}
+	public String getCourthouseName(){
+		return courthouseName;
+	}
+	public float getNumDaysTrial(){
+		return numDaysTrial;
+	}
+	
+	
+}
---
+++ JB Lawfirm/src/Rejected.java
@@ -0,0 +1,14 @@
+
+public class Rejected {
+	String reason;
+	public Rejected(){
+		super();
+		reason = "";
+	}
+	public void setReason(String r){
+		reason = r;
+	}
+	public String getReason(){
+		return reason;
+	}
+}
