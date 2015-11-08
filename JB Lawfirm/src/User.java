15ee663a8e9bcd516a8f0a018f5e6505428dd5c9
Reenu <Reenu@Reenu-PC.eduroam.gmu.edu>
2015-11-08 13:24:57 -0500

    created user class

---
+++ JB Lawfirm/src/User.java
@@ -0,0 +1,15 @@
+
+public class User {
+
+	String username;
+	String password;
+	
+	public User(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String toString(){
+		return "Username: " + username + "Password: " + password;
+	}
+}
