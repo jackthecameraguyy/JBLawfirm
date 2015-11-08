cbf2d7ba8f18bfdba6eddfafd66634915e56b0e8
Reenu <Reenu@Reenu-PC.eduroam.gmu.edu>
2015-11-08 13:06:43 -0500

    fixed

--- JB Lawfirm/src/Case.java
+++ JB Lawfirm/src/Case.java
@@ -1,40 +1,71 @@
 
 public class Case {
-	
+	//initialize variables
 	int caseNumber;
 	String title;
 	String status;
 	
+	//default constructor
 	public Case(){
-		Plaintiff plaintiff = new Plaintiff();
 		caseNumber = 0;
 		title = "";
 		status = "";
 		
 	}
+	
+	/**	 
+	 * @param caseNum
+	 */
 	public void setCaseNum(int caseNum){
 		caseNumber = caseNum;
 	}
+	
+	/**
+	 * 
+	 * @param title
+	 */
 	public void setTitle(String title){
 		this.title = title;
 	}
-	public void setPlaintiff(){
+	
+	/**
+	 * 
+	 * @param name
+	 * @param phone
+	 * @param email
+	 */
+	public void setPlaintiff(String name, String phone, String email ){
+		
+		Plaintiff plaintiff = new Plaintiff(name, phone, email);
 		
 	}
+	
+	//set the status
+	/**
+	 * 
+	 * @param status
+	 */
 	public void setStatus(String status){
 		this.status = status;
 	}
+	//get casenumber
 	public int getCaseNum(){
 		return caseNumber;
 	}
+	//get title
 	public String getTitle(){
 		return title;
 	}
-	public Plaintiff getPlaintiff(){
-		//return plaintiff;
-	}
+	
+	
+	//public Plaintiff getPlaintiff(){}
+	
+	
 	public String getStatus(){
 		return status;
+	}
+	public String toString(){
+		return "Case Number:" + caseNumber + "Status: " + status + " Title: " + title ;
 	}
 	
 	
