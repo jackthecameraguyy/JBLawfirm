f2c13c9ce620496740977a892d0a5775a668cc08
Melanie Johnson <mjohns35@gmu.edu>
2015-11-08 12:42:23 -0500

    added javadoc comments

--- JB Lawfirm/src/Plaintiff.java
+++ JB Lawfirm/src/Plaintiff.java
@@ -1,35 +1,60 @@
 
 public class Plaintiff {
-	//Hello
 	
 	private String name;
 	private String phone;
 	private String email;
 	
+	/**
+	 * sets the plaintiff's name
+	 * @param name
+	 */
 	public void setName(String name){
 		this.name = name;
 	}
 	
+	/**
+	 * sets the plaintiff's phone number
+	 * @param phone
+	 */
 	public void setPhone(String phone){
 		this.phone = phone;
 	}
 
+	/**
+	 * sets the plaintiff's email address
+	 * @param email
+	 */
 	public void setEmail(String email){
 		this.email = email;
 	}
 	
+	/**
+	 * @return name
+	 */
 	public String getName(){
 		return name;
 	}
 	
+	/**
+	 * @return phone
+	 */
 	public String getPhone(){
 		return phone;
 	}
 	
+	/**
+	 * @return email 
+	 */
 	public String getEmail(){
 		return email;
 	}
 	
+	/**
+	 * returns a string with plaintiff info in format:
+	 * Plaintiff Name: 1234567890, email@email.com
+	 * @return plaintiff
+	 */
 	public String toString(){
 		return name + ": " + phone + ", " + email;
 	}
