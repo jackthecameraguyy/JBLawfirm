03f3803d23c3ac26b71247086707d2860d0e7e3b
Reenu <Reenu@Reenu-PC.eduroam.gmu.edu>
2015-11-08 12:17:09 -0500

    case class

---
+++ JB Lawfirm/src/Case.java
@@ -0,0 +1,43 @@
+
+public class Case {
+	
+	double caseNumber;
+	String title;
+	String status;
+	
+	public Case(){
+		Plaintiff plaintiff = new Plaintiff();
+		caseNumber = 0;
+		title = "";
+		status = "";
+		
+	}
+	public void setCaseNum(int caseNum){
+		caseNumber = caseNum;
+	}
+	public void setTitle(String title){
+		this.title = title;
+	}
+	public void setPlaintiff(){
+		
+	}
+	public void setStatus(String status){
+		this.status = status;
+	}
+	public int getCaseNum(){
+		return caseNumber;
+	}
+	public String getTitle(){
+		return title;
+	}
+	public Plaintiff getPlaintiff(){
+		//return plaintiff;
+	}
+	public String getStatus(){
+		return status;
+	}
+	
+	
+	
+	
+}
